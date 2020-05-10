b47b0a7677b7101b5d97b65ffd66be9c91357816
lazyants <smaxims@gmail.com>
2020-05-10 18:05:36 +0200

    refactoring

--- src/Command/LsCommand.php
+++ src/Command/LsCommand.php
@@ -4,7 +4,6 @@ namespace App\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
 
 class LsCommand extends AbstractCommand
 {
@@ -19,27 +18,12 @@ class LsCommand extends AbstractCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Total servers:</info> ' . $this->config->count());
+        parent::execute($input, $output);
 
-        $this->config->each(
-            function ($hostConfig, $i) use ($output) {
-                $output->writeln('');
-                $output->writeln(sprintf('<info>[%s] Running:</info> %s', $i + 1, $hostConfig['name']));
-
-                $process = new Process(
-                    [
-                        'ssh',
-                        $hostConfig['connection-string'],
-                        'ls -lha',
-                    ]
-                );
-                $process->setTty(Process::isTtySupported());
-                $process->start();
-                $process->wait();
-
-                $output->writeln('<info>Finished:</info> ' . $hostConfig['name']);
-            }
-        );
+        $this
+            ->process('ls -lha')
+            ->outputList($output)
+            ->outputErrors($output);
 
         return 0;
     }
--- src/Command/UpgradeCommand.php
+++ src/Command/UpgradeCommand.php
@@ -4,8 +4,6 @@ namespace App\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\InputStream;
-use Symfony\Component\Process\Process;
 
 class UpgradeCommand extends AbstractCommand
 {
@@ -20,44 +18,16 @@ class UpgradeCommand extends AbstractCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Total servers:</info> ' . $this->config->count());
+        parent::execute($input, $output);
 
-        $this->config->each(
-            function ($hostConfig, $i) use ($output) {
-                $output->writeln('');
-                $output->writeln(sprintf('<info>[%s] Running:</info> %s', $i + 1, $hostConfig['name']));
-
-                $input = new InputStream();
-
-                $process = new Process(
-                    [
-                        'ssh',
-                        $hostConfig['connection-string'],
-                        '-o SendEnv="PASSWORD"',
-                    ],
-                    null,
-                    [
-                        'PASSWORD' => $_ENV['SUDO_PASSWORD'],
-                    ]
-                );
-                $process->setInput($input);
-                $process->start();
-
-                $input->write('echo "startoutputsisteminformation"');
-                $input->write('&& echo $PASSWORD | sudo -S apt-get update');
-                $input->write('&& echo $PASSWORD | sudo -S apt-get -y upgrade');
-                $input->write('&& echo $PASSWORD | sudo -S apt-get -y autoremove');
-
-                $input->close();
-
-                $process->wait();
-
-                $serverOutput = explode('startoutputsisteminformation', $process->getOutput())[1];
-
-                $output->writeln($serverOutput);
-                $output->writeln('<info>Finished:</info> ' . $hostConfig['name']);
-            }
-        );
+        $this
+            ->process(
+                'echo $PASSWORD | sudo -S apt-get update' .
+                '&& echo $PASSWORD | sudo -S apt-get -y upgrade' .
+                '&& echo $PASSWORD | sudo -S apt-get -y autoremove'
+            )
+            ->outputList($output)
+            ->outputErrors($output);
 
         return 0;
     }
