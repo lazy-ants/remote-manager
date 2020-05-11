f3a1c160855d07a3e6922c44a0d28d09bce21550
lazyants <smaxims@gmail.com>
2020-05-11 17:57:30 +0200

    add docker prune command

---
+++ src/Command/DockerPruneCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DockerPruneCommand extends AbstractCommand
+{
+    protected static $defaultName = 'app:docker-prune';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Prune old docker data')
+            ->setHelp('Prune old docker data');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+
+        $this
+            ->process('echo "y" | docker system prune')
+            ->outputList($output)
+            ->outputErrors($output);
+
+        return 0;
+    }
+}
