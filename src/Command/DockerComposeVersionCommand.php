e50b48906b886609dc95426e398370d622fb916b
lazyants <smaxims@gmail.com>
2020-05-12 13:47:06 +0200

    add checking docker compose version

---
+++ src/Command/DockerComposeVersionCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DockerComposeVersionCommand extends AbstractCommand
+{
+    protected static $defaultName = 'app:docker-compose-version';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Get docket compose version')
+            ->setHelp('Get docket compose version');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'Docker compose version']);
+
+        $this
+            ->process('docker-compose -v')
+            ->outputTable($output, $table)
+            ->outputErrors($output);
+
+        return 0;
+    }
+}
