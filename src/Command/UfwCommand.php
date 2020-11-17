135ba42c967f8d9f025aa8032d3d821e8a2fbd1d
lazyants <smaxims@gmail.com>
2020-11-17 20:55:54 +0100

    add arguments to the ufw command

--- src/Command/UfwCommand.php
+++ src/Command/UfwCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -15,32 +16,38 @@ class UfwCommand extends AbstractCommand
     {
         parent::configure();
 
-        $this
-            ->setDescription('Get ufw status [need sudo]')
-            ->setHelp('Get ufw status [need sudo]');
+        $this->setDescription('Get ufw status [need sudo]')->setHelp('Get ufw status [need sudo]')->addArgument('arg', InputArgument::OPTIONAL, 'ufw command arguments');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         parent::execute($input, $output);
 
-        $table = new Table($output);
-        $table->setHeaders(['Name', 'UFW Status']);
+        $arg = '';
+        if (is_string($input->getArgument('arg'))) {
+            $arg = $input->getArgument('arg');
+        }
 
-        $this->process('echo $PASSWORD | sudo -S ufw status');
+        if (!empty($arg)) {
+            $this->process('echo $PASSWORD | sudo -S ufw ' . $arg)
+                ->outputList($output)
+                ->outputErrors($output);
+        } else {
+            $table = new Table($output);
+            $table->setHeaders(['Name', 'UFW Status']);
 
-        $this->results->transform(
-            function ($item) {
+            $this->process('echo $PASSWORD | sudo -S ufw status');
+
+            $this->results->transform(function ($item) {
                 preg_match("/Status: (.*)\n/m", $item['value'], $matches);
 
                 $item['value'] = isset($matches[1]) ? $matches[1] : '';
 
                 return $item;
-            }
-        );
+            });
 
-        $this->outputTable($output, $table)
-            ->outputErrors($output);
+            $this->outputTable($output, $table)->outputErrors($output);
+        }
 
         return 0;
     }
